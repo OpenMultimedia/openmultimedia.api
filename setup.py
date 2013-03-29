9cd9b73bb042ed64835177f3b11b5cff587813fe
hvelarde <hector.velarde@gmail.com>
2013-03-29 12:38:38 -0300

    fix package distribution

--- setup.py
+++ setup.py
@@ -1,29 +1,37 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
+
 import os
 
 version = '1.0'
+long_description = open("README.rst").read() + "\n" + \
+    open(os.path.join("docs", "INSTALL.rst")).read() + "\n" + \
+    open(os.path.join("docs", "CREDITS.rst")).read() + "\n" + \
+    open(os.path.join("docs", "HISTORY.rst")).read()
 
 setup(name='openmultimedia.api',
       version=version,
       description="API para interactuar con el API de OpenMultimedia",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Framework :: Plone :: 4.1",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Operating System :: OS Independent",
-        "Programming Language :: JavaScript",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Topic :: Office/Business :: News/Diary",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Intended Audience :: End Users/Desktop",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+          "Operating System :: OS Independent",
+          "Programming Language :: JavaScript",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Office/Business :: News/Diary",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='plone api rest openmultimedia',
       author='Franco Pellegrini',
       author_email='frapell@ravvit.net',
@@ -35,19 +43,19 @@ setup(name='openmultimedia.api',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-        'setuptools',
-        'plone.app.dexterity',
-        'plone.behavior',
-        'plone.directives.form',
-        'zope.schema',
-        'zope.interface',
-        'zope.component',
-        'rwproperty',
-        'collective.z3cform.widgets',
-        ],
+          'setuptools',
+          'plone.app.dexterity',
+          'plone.behavior',
+          'plone.directives.form',
+          'zope.schema',
+          'zope.interface',
+          'zope.component',
+          'rwproperty',
+          'collective.z3cform.widgets',
+      ],
       extras_require={
-        'test': ['plone.app.testing'],
-        },
+          'test': ['plone.app.testing'],
+      },
       entry_points="""
       [z3c.autoinclude.plugin]
       target = plone
